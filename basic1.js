4b444deddc5e743473b45c0220a38987454361c0
Sagar Haldhar <sagarhaldhar76@gmail.com>
2025-12-06 23:58:38 +0530

    added new files

--- basic1.js
+++ basic1.js
@@ -11,4 +11,6 @@ console.log(obj1.name); // undefined
 
 let obj2;
 //console.log(obj2.name); // TypeError: Cannot read properties of undefined (reading 'name')
-console.log(obj2?.name);
+
+console.log(obj2?.name); // undefined
+// Ye check krega ki obj2 exist karta hai (uski value undefined to nahi hai) ya nahi agar exist karta hai to uski value print karega agar nahi to undefined print karega.
